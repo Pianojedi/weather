61c69f9e693c20386b62ce5955e3c4cd2d263002
Pianojedi <pianojedi124@gmail.com>
2025-01-21 11:44:46 -0600

    Added API key

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        //Initializing call for Weather Data
+        String apiKey = "64803804e1cacf100b3754eac54e49c4";
+        String city = "Pensacola";
+        String urlString = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey;
+
+
+
+
+
     }
+
 }
